f10a591fd7282f82bd975476c54e32dbe002bf57
Jared Butler <chromeoverload@gmail.com>
2021-02-07 20:53:35 -0500

    successful contractions screen scroll (ugly formatting)

--- app/src/main/java/com/example/heratale_app/Contractions.java
+++ app/src/main/java/com/example/heratale_app/Contractions.java
@@ -1,4 +1,26 @@
 package com.example.heratale_app;
 
-public class Contractions {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Contractions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contractions_slidedeck);
+
+        /*Button contractions_slidedeck = findViewById(R.id.button7);
+        contractions_slidedeck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WordStructuresMainActivity.this, WordStructuresMainActivity.class));
+            }
+        });*/
+    }
+
 }
--- app/src/main/java/com/example/heratale_app/WordStructuresMainActivity.java
+++ app/src/main/java/com/example/heratale_app/WordStructuresMainActivity.java
@@ -2,7 +2,10 @@ package com.example.heratale_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WordStructuresMainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class WordStructuresMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_structures_main);
+
+        Button contractions_slidedeck = findViewById(R.id.button7);
+        contractions_slidedeck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WordStructuresMainActivity.this, Contractions.class));
+            }
+        });
     }
+
+
 }
