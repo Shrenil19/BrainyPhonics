af6f192632f79f7e8ba2d3d93a1687537f788491
lcvg3 <lcvg3@gatech.edu>
2021-02-24 09:32:44 -0500

    Contractions Check Progress & Quiz
    Created contactions check progress page as well as as quiz page for 'am, are' contractions. Connected home, quiz and check progress buttons of the contractions "am, are" subsection to their respective pages. Deleted back button since android apps innately provide that feature.

---
+++ app/src/main/java/com/example/heratale_app/Quiz.java
@@ -0,0 +1,13 @@
+package com.example.heratale_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Quiz extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz);
+    }
+}
