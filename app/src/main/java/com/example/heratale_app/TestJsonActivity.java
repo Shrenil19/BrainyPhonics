046352ec325d5e59521d1c5b28dd08af0e060a98
Jared Butler <chromeoverload@gmail.com>
2021-02-25 21:19:09 -0500

    final fix for testing json

--- app/src/main/java/com/example/heratale_app/TestJsonActivity.java
+++ app/src/main/java/com/example/heratale_app/TestJsonActivity.java
@@ -3,6 +3,7 @@ package com.example.heratale_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import com.example.heratale_app.json.JsonHelper;
 
@@ -11,10 +12,12 @@ public class TestJsonActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contractions_slidedeck);
+        setContentView(R.layout.activity_test_json);
 
         JsonHelper helper = new JsonHelper("test1", this);
         TextView text = findViewById(R.id.textView);
+        Log.d("text", text.toString());
+        Log.d("helper", helper.readFocusItem(1));
 
         text.setText(helper.readFocusItem(1));
     }
