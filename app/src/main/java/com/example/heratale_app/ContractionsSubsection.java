f34bc87a1fba93df016c2271f595c2f4b03bf163
lcvg3 <lcvg3@gatech.edu>
2021-02-26 12:04:02 -0500

    Adding contractions menu
    Making a contractions menu in order to access all subsections. Currently linked to the "am, are" subsection.

--- app/src/main/java/com/example/heratale_app/ContractionsSubsection.java
+++ app/src/main/java/com/example/heratale_app/ContractionsSubsection.java
@@ -1,11 +1,9 @@
-
 package com.example.heratale_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -13,18 +11,26 @@ import android.widget.ImageButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class Contractions extends AppCompatActivity {
+public class ContractionsSubsection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.contractions_menu);
+        setContentView(R.layout.activity_contractions_slidedeck);
 
         ImageButton home = findViewById(R.id.home);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Contractions.this, MenuActivity.class));
+                startActivity(new Intent(ContractionsSubsection.this, MenuActivity.class));
+            }
+        });
+
+        ImageButton quiz = findViewById(R.id.quiz);
+        quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ContractionsSubsection.this, Quiz.class));
             }
         });
 
@@ -32,16 +38,7 @@ public class Contractions extends AppCompatActivity {
         check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Contractions.this, ContractionsCheck.class));
-            }
-        });
-
-        Button m_re = findViewById(R.id.m_re);
-        m_re.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                startActivity(new Intent(Contractions.this, ContractionsSubsection.class));
-                return false;
+                startActivity(new Intent(ContractionsSubsection.this, ContractionsCheck.class));
             }
         });
     }
